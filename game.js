a22322000995cfda66e63451bd2039893471f849
Zachary Saile <zwsaile@gmail.com>
2022-03-02 15:09:32 -0700

    Refactor Game and Player methods for readability

--- game.js
+++ game.js
@@ -9,41 +9,41 @@ class Game {
     this.players = [humanPlayer, compPlayer];
   };
 
+  changeHumanScore() {
+    this.players[0].wins += 1;
+    renderScore();
+    return `${this.players[0].name} wins! Nice!`;
+  };
+
+  changeCompScore() {
+    this.players[1].wins += 1;
+    renderScore();
+    return `${this.players[1].name} wins! Nice!`;
+  };
+
   countHumanWin() {
     if (this.playerChoices[0] === "rock" && this.playerChoices[1] === "scissors") {
-      this.players[0].wins += 1;
-      renderScore(humanScore);
-      return `${this.players[0].name} wins! Nice!`;
+      this.changeHumanScore();
     } else if (this.playerChoices[0] === "scissors" && this.playerChoices[1] === "paper") {
-      this.players[0].wins += 1;
-      renderScore(humanScore);
-      return `${this.players[0].name} wins! Nice!`;
+      this.changeHumanScore();
     } else if (this.playerChoices[0] === "paper" && this.playerChoices[1] === "rock") {
-      this.players[0].wins += 1;
-      renderScore(humanScore);
-      return `${this.players[0].name} wins! Nice!`;
+      this.changeHumanScore();
     };
   };
 
   countCompWin() {
     if (this.playerChoices[1] === "rock" && this.playerChoices[0] === "scissors") {
-      this.players[1].wins += 1;
-      renderScore();
-      return `${this.players[1].name} wins! Nice!`;
+      this.changeCompScore()
     } else if (this.playerChoices[1] === "scissors" && this.playerChoices[0] === "paper") {
-      this.players[1].wins += 1;
-      renderScore();
-      return `${this.players[1].name} wins! Nice!`;
+      this.changeCompScore()
     } else if (this.playerChoices[1] === "paper" && this.playerChoices[0] === "rock") {
-      this.players[1].wins += 1;
-      renderScore();
-      return `${this.players[1].name} wins! Nice!`;
+      this.changeCompScore()
     };
   };
 
 
   // countDraw() {
-  //   if (playerChoices.humanChoice === playerChoices.compChoice)
+  //   if (playerChoices[0] === playerChoices.[1])
   //   return `Dang! A draw!`
   // };
 
